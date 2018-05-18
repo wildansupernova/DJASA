88c20f5d39dd6f6bb866affdf4534da72d6aecbc
priagungs <32959030+priagungs@users.noreply.github.com>
2018-05-18 09:10:38 +0700

    initialize node server

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 // insert code node js below
+var express = require('express');
+var app = express();     
+var bodyParser = require('body-parser');
+var sql = require('mysql');
+
+
+app.use(express.static(__dirname + '../public'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+
+app.listen(8080);
+console.log("App is listening on 8080");
+
 const sequelize = new Sequelize('database','username','password',{
 	dialect: 'mysql',
 	host: "my.server.tld",
 	port: 9080
+});
+
+// insert api response here
+
+// start app
+app.get('*', function(req, res){
+    res.sendFile('./public/index.html');
 })
