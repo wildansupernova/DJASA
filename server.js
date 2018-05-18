a1d058375069dec49c94de735c78dc5aa5c2d95c
Aufa <muhammad.aufa.helfiandri@gmail.com>
2018-05-18 16:10:53 +0700

    SQL

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "unit52",
-    database: "tubes_basdat"
+    password: "",
+    database: "Djasa"
 })
 
 
