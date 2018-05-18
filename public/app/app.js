d0dcd3e1f614d68e165bdaec25e510718c0fabbc
priagungs <32959030+priagungs@users.noreply.github.com>
2018-05-18 09:37:46 +0700

    initialize angularjs

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,15 @@
 // insert angular js code below
+
+var djasaApp = angular.module('djasaApp', []);
+
+function loginController($scope, $http) {
+    var submitLogin = function() {
+
+    }
+}
+
+function signupController($scope, $http) {
+    var submitSignup = function() {
+        
+    }
+}
