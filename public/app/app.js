d4cc45fffa1eda3ef8a70a5ff83249884b704ae2
priagungs <32959030+priagungs@users.noreply.github.com>
2018-05-18 13:20:53 +0700

    1.20pm

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,33 @@
 // insert angular js code below
 
-var djasaApp = angular.module('djasaApp', []);
+var djasaApp = angular.module('djasaApp', ['ngRoute']);
+
+djasaApp.config(function($routeProvider){
+    $routeProvider
+        .when("/", {
+            templateUrl : "landing.html",
+            controller: "landingController"
+        })
+        .when("/home", {
+            templateUrl: "dashboard.html",
+            controller: "dashboardController"
+        })
+        .when("/transaction", {
+            templateUrl: "transaction.html",
+            controller: "transactionController"
+        })
+        .when("/addservice", {
+            templateUrl: "addPage.html",
+            controller: "addServiceController"
+        })
+        .when("/myservice", {
+            templateUrl: "myservice.html", //sesuaikan namanya
+            controller: "myServiceController"
+        })
+        .otherwise({
+            template : "<h1>404 Page Not Found</h1>"
+        })
+})
 
 function loginController($scope, $http) {
     var submitLogin = function() {
@@ -12,5 +39,19 @@ function signupController($scope, $http) {
     var submitSignup = function() {
 
     }
+}
+
+function addServiceController($scope, $http) {
+    var submitNewService = function() {
+
+    }
+}
+
+function navbarController($scope, $http) {
+    var logOut = function() {
+
+    }
+
+    
 }
 
